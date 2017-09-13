c6d2bf93f79149694737127ff6b84be24ce30437
Henrik Sunden <hswd.development@gmail.com>
2017-09-13 15:10:15 +0300

    copy from src to app added

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync'); /* import browser sync into gulp file */
 var reload = browserSync.reload; /*  */
+var autoprefixer = require('gulp-autoprefixer'); /* import gulp autoprefixer */
 
 var SOURCEPATHS = {
-	sassSource : 'src/scss/*.scss' /* add everything with .scss */
+	sassSource : 'src/scss/*.scss', /* add everything with .scss */
+	htmlSource : 'src/*.html'
 }
 var APPPATH = {
 	root: 'app/',
@@ -12,24 +14,33 @@ var APPPATH = {
 	js : 'app/js'
 }
 
-/* make taks called "sass" functioning and compiling sass to css */
+/* make taks called "sass" functioning and compiling sass to css, pipe order has to be right */
 gulp.task('sass', function(){
-	return gulp.src('SOURCEPATHS.sassSource')
-		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-		.pipe(gulp.dest('APPPATH.css'))
+	return gulp.src(SOURCEPATHS.sassSource)
+		.pipe(autoprefixer())
+		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) /* expanded can also be nested, compressed or compact */
+		.pipe(gulp.dest(APPPATH.css)); /* destination */
 });
 
-gulp.task('serve', ['sass'], function(){
-	browserSync.init ([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
+/* copy all html changes or files from src to app */
+gulp.task('copy', function() {
+	gulp.src(SOURCEPATHS.htmlSource)
+		.pipe(gulp.dest(APPPATH.root))
+});
+
+/* run task called "serve" and listen to all css, html, js files. add to browserSync */
+gulp.task('serve', ['sass'], function() {
+	browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
 		server : {
 			baseDir : APPPATH.root
 		}
 	})
 });
 
-/* run task that automatically updates scss files also*/
-gulp.task('watch', ['serve', 'sass'], function() {
+/* run task that automatically updates scss files also by browserSync*/
+gulp.task('watch', ['serve', 'sass', 'copy'], function() {
 	gulp.watch([SOURCEPATHS.sassSource], ['sass']);
+	gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
 } );
 
 /* run task called default and run also every other task what is in here */

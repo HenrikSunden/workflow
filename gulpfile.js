dacc7b7007ab597d3d845400cba9b6de49bfb3b1
Henrik Sunden <hswd.development@gmail.com>
2017-09-13 12:19:50 +0300

    browser sync added

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,36 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync'); /* import browser sync into gulp file */
+var reload = browserSync.reload; /*  */
 
+var SOURCEPATHS = {
+	sassSource : 'src/scss/*.scss' /* add everything with .scss */
+}
+var APPPATH = {
+	root: 'app/',
+	css : 'app/css',
+	js : 'app/js'
+}
 
 /* make taks called "sass" functioning and compiling sass to css */
 gulp.task('sass', function(){
-	return gulp.src('src/scss/app.scss')
+	return gulp.src('SOURCEPATHS.sassSource')
 		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-		.pipe(gulp.dest('app/css'))
+		.pipe(gulp.dest('APPPATH.css'))
 });
 
-/* make */
-gulp.task('default' , ['sass']);
+gulp.task('serve', ['sass'], function(){
+	browserSync.init ([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
+		server : {
+			baseDir : APPPATH.root
+		}
+	})
+});
+
+/* run task that automatically updates scss files also*/
+gulp.task('watch', ['serve', 'sass'], function() {
+	gulp.watch([SOURCEPATHS.sassSource], ['sass']);
+} );
+
+/* run task called default and run also every other task what is in here */
+gulp.task('default' , ['watch']);
